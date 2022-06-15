c4bf1c1f7b39487a5bafcc7b21afa292c76fd38f
ssejal8 <104586650+ssejal8@users.noreply.github.com>
2022-06-15 13:12:23 +0530

    Add files via upload

---
+++ src/Data.js
@@ -0,0 +1,58 @@
+let shopItemsData = [
+  {
+    id: "whiteTop",
+    name: "White Top",
+    price: 500,
+    desc: "White casual top for a summery day.",
+    img: "images/white-top.jpg",
+  },
+  {
+    id: "blackNetSkirt",
+    name: "Black Net Skirt",
+    price: 600,
+    desc: "Black Skirt for office party,casual day.",
+    img: "images/Skirt.jpg",
+  },
+  {
+    id: "loopEaarings",
+    name: "Loop Earrings",
+    price: 250,
+    desc: "Loop Earrings for a classy look.",
+    img: "images/loop-earrings.webp",
+  },
+  {
+    id: "butterflyEarrings",
+    name: "Butterfly Earrings",
+    price: 300,
+    desc: "Cute Butterfly Earrings for any occassion.",
+    img: "images/butterfly-earrings.jpeg",
+  },
+  {
+    id: "Dress",
+    name: "Peach Dress",
+    price: 900,
+    desc: "Peach Dress for any occasion.",
+    img: "images/dress.jpg",
+  },
+  {
+    id: "jeans",
+    name: "Faded Blue Jeans",
+    price: 1200,
+    desc: "Joggers jeans in blue faded design.",
+    img: "images/jeans.jpeg",
+  },
+  {
+    id: "Long Shoes",
+    name: "Long Black Shoes",
+    price: 2000,
+    desc: "Long Black Stylish Leather Boots.",
+    img: "images/blacklongshoes.jpg",
+  },
+  {
+    id: "Slippers",
+    name: "Slippers",
+    price: 500,
+    desc: "Comfaortable Flats for dailywear.",
+    img: "images/flats.jpg",
+  },
+];
